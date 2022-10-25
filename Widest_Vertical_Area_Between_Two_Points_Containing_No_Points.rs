509f1dfe994fe40cf586cae6f1e23b0fe240f65f
Parikalp-Bhardwaj <parikalp.456@gmail.com>
2022-10-25 22:16:53 +0530

    dsa-with-golang-rust-python

---
+++ Widest_Vertical_Area_Between_Two_Points_Containing_No_Points.rs
@@ -0,0 +1,19 @@
+fn main(){
+    let points:Vec<Vec<i32>> = vec![vec![8,7],vec![9,9],vec![7,4],vec![9,7]];
+    println!("{:?} ",max_width_of_vertical_area(points));
+}
+
+pub fn max_width_of_vertical_area(points:Vec<Vec<i32>>) ->i32{
+    let mut v1:Vec<i32> = vec![0;points.len()];
+    for i in 0..points.len(){
+        v1[i]=points[i][0];
+    }
+    v1.sort();
+    let mut max = 0;
+    for i in 1..v1.len(){
+        if v1[i] - v1[i-1] > max{
+            max = v1[i] - v1[i-1];
+        }
+    }
+    return max
+}
