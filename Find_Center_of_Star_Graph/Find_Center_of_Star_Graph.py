f741887fdb20ffee811d8b4fe01c6e94573cd026
Parikalp-Bhardwaj <parikalp.456@gmail.com>
2023-05-02 00:25:24 +0530

    dsa-with-golang-and-python

--- Find_Center_of_Star_Graph/Find_Center_of_Star_Graph.py
+++ Find_Center_of_Star_Graph/Find_Center_of_Star_Graph.py
@@ -1,15 +1,21 @@
-
-edges = [[1,2],[2,3],[4,2]]
+from collections import defaultdict
 def findCenter(edges):
-    s1 = set()
-    for i,j in edges:
-        if i in s1:
-            return i
-        if j in s1:
-            return j
-        s1.add(i)
-        s1.add(j)
-        
+    matrix = defaultdict(list)
+    for edge in edges:
+        u = edge[0]
+        v = edge[1]
+        matrix[u].append(v)
+        matrix[v].append(u)
+
+    l = len(matrix)-1
+    print(matrix,l)
+    for key in matrix.keys():
+        if len(matrix[key]) == l:
+            return key
 
 
+
+
+# edges = [[1,2],[2,3],[4,2]]
+edges = [[1,2],[5,1],[1,3],[1,4]]
 print(findCenter(edges))
